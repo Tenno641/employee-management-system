228118a82de24b75f4b7e7b8fb179daa746db98f
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2022-12-11 21:05:52 +0200

    #initializing student form

--- Project.py
+++ Project.py
@@ -8,3 +8,11 @@ window.geometry('1200x600+50+50')
 window.resizable(False, False)
 window.configure(bg='#36393e')
 
+#initializing student form
+infoForm = Frame(window, bg='#1e2124')
+infoForm.place(x=10, y=10, width=300, height=580)
+
+#initializing the student form title
+titleForm = Label(infoForm, text='Students Informations', font=('', 13, 'bold'), fg='#ffffff', bg='#1e2124')
+titleForm.place(x=10, y=10, height=50)
+
