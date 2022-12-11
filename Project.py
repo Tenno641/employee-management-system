5bd94c681d537956bdb30cbac226eed510170e89
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2022-12-11 21:05:18 +0200

    #initializing the project's window

--- Project.py
+++ Project.py
@@ -1,3 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 
+#initializing the project's window
+window = Tk()
+window.title('Students Management System')
+window.geometry('1200x600+50+50')
+window.resizable(False, False)
+window.configure(bg='#36393e')
+
