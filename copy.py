61f13503fcbeba4adbda8cd7291ba5f79c63abf6
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2022-12-12 20:02:05 +0200

    Initial commit

---
+++ copy.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+
+#initializing program window
+window = Tk()
+window.title("Students Management System")
+window.geometry("1200x700+0+10")
+window.configure(bg='green')
+window.resizable(False, False)
+
+
+
+#initializing entries form
+entriesForm = Frame(window, bg="red")
+entriesForm.place(x=0, y=0, width=350, height=700)
+
+
+
+
+
+
+
+
+
+window.mainloop()
