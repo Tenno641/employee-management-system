2374823b54f0ca9d08ada5e88f6911a22b5ed6d9
Tenno641 <120097440+Tenno641@users.noreply.github.com>
2022-12-13 03:27:50 +0200

    Update db.py

--- db.py
+++ db.py
@@ -5,41 +5,40 @@ class Database:
         self.con = sqlite3.connect(db)
         self.cur = self.con.cursor()
         sql = """
-        CREATE TABLE IF NOT EXISTS employees(
+        CREATE TABLE IF NOT EXISTS students(
             id Integer Primary Key,
+            code text,
             name text,
-            age text,
-            city text,
             email text,
             gender text,
-            contact text,
-            address text
+            phone text,
+            city text
         )
         """
         self.cur.execute(sql)
         self.con.commit()
 
     # Insert Function
-    def insert(self, name, age, city, email, gender, contact, address):
-        self.cur.execute("insert into employees values (NULL,?,?,?,?,?,?,?)",
-                         (name, age, city, email, gender, contact, address))
+    def insert(self, code, name, email, gender, phone, city):
+        self.cur.execute("insert into students values (NULL,?,?,?,?,?,?)",
+                         (code, name, email, gender, phone, city))
         self.con.commit()
 
     # Fetch All Data from DB
     def fetch(self):
-        self.cur.execute("SELECT * from employees")
+        self.cur.execute("SELECT * from students")
         rows = self.cur.fetchall()
         # print(rows)
         return rows
 
     # Delete a Record in DB
     def remove(self, id):
-        self.cur.execute("delete from employees where id=?", (id,))
+        self.cur.execute("delete from students where id=?", (id,))
         self.con.commit()
 
     # Update a Record in DB
-    def update(self, id, name, age, city, email, gender, contact, address):
+    def update(self, id, code, name, email, gender, phone, city):
         self.cur.execute(
-            "update employees set name=?, age=?, city=?, email=?, gender=?, contact=?, address=? where id=?",
-            (name, age, city, email, gender, contact, address, id))
+            "update students set code=?, name=?, email=?, gender=?, phone=?, city=? where id=?",
+            (code, name, email, gender, phone, city, id))
         self.con.commit()
